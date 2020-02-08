aa5422b03c63529fe14467e973cea1fb4e9ecfd6
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-07 17:07:32 -0800

    Make prettier

--- client/src/components/edit/image-overlay/Box.js
+++ client/src/components/edit/image-overlay/Box.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { useDrag } from 'react-dnd'
-import ItemTypes from './ItemTypes'
+import React from "react";
+import { useDrag } from "react-dnd";
+import ItemTypes from "./ItemTypes";
 
 const style = {
-  position: 'absolute',
-  cursor: 'move',
-}
+  position: "absolute",
+  cursor: "move"
+};
 const Box = ({ left, top, imageURL }) => {
   const [{ isDragging }, drag] = useDrag({
     item: { left, top, type: ItemTypes.BOX },
     collect: monitor => ({
-      isDragging: monitor.isDragging(),
-    }),
-  })
+      isDragging: monitor.isDragging()
+    })
+  });
   if (isDragging) {
-    return <div ref={drag} />
+    return <div ref={drag} />;
   }
   return (
     <div ref={drag} style={{ ...style, left, top }}>
-      <img src={imageURL} />
+      <img src={imageURL} style={{ height: "400px" }} id="image-overlay"/>
     </div>
-  )
-}
-export default Box
+  );
+};
+export default Box;
